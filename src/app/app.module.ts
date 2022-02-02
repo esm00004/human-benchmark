cdce581d4dc73a17581910b7234cb9002793d9ad
Evan <esm00004@mix.wvu.edu>
2022-02-01 19:23:02 -0500

    Updated folder import for navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TopNavBarComponent } from './header/topnavbar.component';
+import { TopNavBarComponent } from './Navigation/header/topnavbar.component';
 import { TestsCardComponent } from './tests/tests.card.component';
 
 @NgModule({
